6a3956a9d67ebca572593d8aef443b08e947c9c4
phazzzed <chris.coburn.1987@protonmail.ch>
2022-04-21 12:35:42 +1000

    v2
    rewrote this function to properly iterate through devices

--- get_facts.py
+++ get_facts.py
@@ -6,19 +6,18 @@ import uname_pass
 UID = uname_pass.username 
 PWD = uname_pass.password
 
-acx = ['10.126.128.4', '10.126.128.5', '10.126.144.4', '10.126.144.5']
-mx = [ '10.126.128.1', '10.126.128.2', '10.126.144.1', '10.126.144.2']
+array_device_list = []
 
-for current_device in mx:
-     current_router = Device(host=current_device, password=PWD, user=UID, normalize=True)
+file_device_list = open('juniper_model_all.list', 'r')
+for line in file_device_list:
+     line = str(line).split(' ')
+     array_device_list.append({'hostname':line[0], 'ip_address':str(line[1]).strip()})
 
+for i in range(len(array_device_list)):
      try:
-          current_router.open()
-     except ConnectError as err:
-          print("main() - cannot connect to device: {0}" .format(err))
+          with Device(host=array_device_list[i]['ip_address'], password=PWD, user=UID, normalize=True) as current_device:
+               pprint('main() - facts about ' + str(current_device.facts['hostname']) + ' are:\n')
+               pprint(current_device.facts)
+     except Exception as e:
+          print ('thrown exception: ' + str(e))
           continue
-
-     pprint('main() - facts about ' + str(current_router.facts['hostname']) + ' are: ' + str(current_router.facts))
-     current_router.close()
-     pprint('main() - closed connection to: ' + str(current_router.facts['hostname']))
-
